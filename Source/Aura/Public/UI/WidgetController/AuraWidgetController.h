3f08c2d6b79916e6be68b7b30f0cfca304535948
Rimor <2590548416@qq.com>
2025-05-01 20:39:17 +0800

    Listening for Attribute Changes

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
 #include "AuraWidgetController.generated.h"
 
 class UAbilitySystemComponent;
@@ -48,6 +47,8 @@ public:
 	void SetWidgetControllerParams(const FWidgetControllerParams& WCParams);
 	
 	virtual void BroadcastInitialValues();
+
+	virtual void BindCallbackToDependencies();
 	
 protected:
 	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
