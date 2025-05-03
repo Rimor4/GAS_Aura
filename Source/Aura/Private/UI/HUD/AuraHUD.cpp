4fbad4bad387b1759a8a8f205d71d76b0fa174e2
Rimor <2590548416@qq.com>
2025-05-03 15:26:37 +0800

    fix HUD初始化时OverlayWidgetClass检出异常

--- Source/Aura/Private/UI/HUD/AuraHUD.cpp
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -9,7 +9,7 @@
 
 void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS)
 {
-	checkf(OverlayWidgetClass, TEXT("Overlay Widget Class uninitialized, please fill out BP_AuraHUD"));
+	checkf(!OverlayWidgetClass.IsNull(), TEXT("Overlay Widget Class unassigned, please fill out BP_AuraHUD"));
 	checkf(OverlayWidgetControllerClass,
 	       TEXT("Overlay Widget Controller Class uninitialized, please fill out BP_AuraHUD"));
 	const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
