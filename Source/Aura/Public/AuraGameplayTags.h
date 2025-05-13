fb54a690c77c7239752dff6157f8dd75039719b7
Rimor <2590548416@qq.com>
2025-05-13 21:56:25 +0800

    Gameplay Tags Singleton

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,24 @@
+// Copyright Lucas Wang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ *
+ * Singleton containing native Gameplay Tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+
+	static void InitializeNativeGameplayTags();
+	
+private:
+	static FAuraGameplayTags GameplayTags;
+	
+};
