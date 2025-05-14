d00cd283756593f7b7eafb228963afcf7e0b69e0
Rimor <2590548416@qq.com>
2025-05-14 22:11:50 +0800

    Responding to Attribute Changes

--- Source/Aura/Public/UI/WidgetController/AttributeMenuController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuController.h
@@ -6,6 +6,8 @@
 #include "UI/WidgetController/AuraWidgetController.h"
 #include "AttributeMenuController.generated.h"
 
+struct FGameplayTag;
+struct FGameplayAttribute;
 class UAttributeInfo;
 struct FAuraAttributeInfo;
 
@@ -27,4 +29,7 @@ public:
 protected:
 	UPROPERTY(EditDefaultsOnly)
 	TObjectPtr<UAttributeInfo> AttributeInfo;
+
+private:
+	void BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const;
 };
