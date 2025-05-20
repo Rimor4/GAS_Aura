8103c0757e3370553d5a9e002174c37c04c3851e
Rimor <2590548416@qq.com>
2025-05-20 23:38:24 +0800

    Aura Projectile Spell

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,14 @@
+// Copyright Lucas Wang
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	
+}
