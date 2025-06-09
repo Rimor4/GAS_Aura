bf51e2f75775f4c6b5b9adb850d3bb4dff6d3c21
Rimor <2590548416@qq.com>
2025-06-09 11:27:18 +0800

    Aura Ability System Globals

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,12 @@
+// Copyright Lucas Wang
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
