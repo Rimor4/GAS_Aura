93a8dfa35997372589df7bd3ead20af3d2b73240
Rimor <2590548416@qq.com>
2025-06-10 18:33:27 +0800

    Behavior Tree Service

---
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -0,0 +1,12 @@
+// Copyright Lucas Wang
+
+
+#include "AI/BTService_FindNearestPlayer.h"
+
+
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	
+}
