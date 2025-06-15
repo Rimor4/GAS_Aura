c3ad44479da41c2620b5744902fb439e609658bc
Rimor <2590548416@qq.com>
2025-06-15 21:42:24 +0800

    Playing the Ranged Attack Montage

--- Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraDamageGameplayAbility.cpp
@@ -20,3 +20,12 @@ void UAuraDamageGameplayAbility::CauseDamage(AActor* TargetActor)
 	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 	GetAbilitySystemComponentFromActorInfo()->ApplyGameplayEffectSpecToTarget(*DamageSpecHandle.Data.Get(), TargetASC);
 }
+
+FTaggedMontage UAuraDamageGameplayAbility::GetRandomTaggedMontage(const TArray<FTaggedMontage>& TaggedMontages)
+{
+	if (int32 RandomIndex = FMath::RandRange(0, TaggedMontages.Num() - 1); TaggedMontages.IsValidIndex(RandomIndex))
+	{
+		return TaggedMontages[RandomIndex];
+	}
+	return FTaggedMontage();
+}
